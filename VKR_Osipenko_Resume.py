72e96858180cb7e9ea860ac99bcba3459ef63276
aerissa <106761692+aerissa@users.noreply.github.com>
2022-06-02 18:43:19 +0300

    Add files via upload

---
+++ VKR_Osipenko_Resume.py
@@ -0,0 +1,431 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+get_ipython().system('pip install fake_useragent')
+get_ipython().system('pip install pyopenssl ndg-httpsclient pyasn1')
+
+
+# In[6]:
+
+
+import re
+import requests
+import fake_useragent
+import pandas as pd
+import time
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+from collections import Counter
+from os import name
+from bs4 import BeautifulSoup
+from randan.descriptive_statistics import ScaleStatistics
+
+
+# In[187]:
+
+
+def get_links(text):
+    ua =fake_useragent.UserAgent()
+    res = requests.get(
+        url=f"https://hh.ru/search/resume?area=113&relocation=living_or_relocation&gender=unknown&text={text}&isDefaultArea=true&exp_period=all_time&logic=normal&pos=full_text&fromSearchLine=false&search_period=0",
+        headers={"user-agent":ua.random}
+    )
+    if res.status_code != 200:
+        return
+    soup = BeautifulSoup(res.content, "lxml")
+    try:
+        page_count = int(soup.find("div",attrs={"class":"pager"}).find_all("span",recursive=False)[-1].find("a").find("span").text)
+    except:
+        return
+    for page in range(page_count):
+        try:
+            res = requests.get(
+                url=f"https://hh.ru/search/resume?area=113&relocation=living_or_relocation&gender=unknown&text={text}&isDefaultArea=true&exp_period=all_time&logic=normal&pos=full_text&fromSearchLine=false&search_period=0&page={page}",
+                headers={"user-agent":ua.random}
+            )
+            if res.status_code == 200:
+                soup = BeautifulSoup(res.content, "lxml")
+                for a in soup.find_all("a",attrs={"class":"resume-search-item__name"}):
+                    yield f'https://hh.ru{a.attrs["href"].split("?")[0]}'
+        except Exception as e:
+            print(f"{e}")
+        time.sleep(1)
+    print(page_count)
+
+
+# In[188]:
+
+
+def get_resume(link):
+    ua = fake_useragent.UserAgent()
+    data = requests.get(
+        url=link,
+        headers={"user-agent":ua.random}
+    )
+    if data.status_code != 200:
+        return
+    soup = BeautifulSoup(data.content, "lxml")
+    try:
+        name = soup.find(attrs={"class":"resume-block__title-text"}).text
+    except:
+        name = ""
+    try:
+        spec = soup.find_all("div", attrs={"class":"bloko-gap bloko-gap_bottom"})[1].text
+    except:
+        spec = []
+    try:
+        salary = soup.find(attrs={"class":"resume-block__title-text_salary"}).text.replace("\u2009","").replace("\xa0"," ")
+    except:
+        salary = ""
+    try:
+        exp = soup.find(attrs={"class":"resume-block__title-text resume-block__title-text_sub"}).text.replace("&nbsp;","").replace("\xa0"," ")
+    except:
+        exp = ""
+    try:
+        tags = [tag.text for tag in soup.find(attrs={"class":"bloko-tag-list"}).find_all("span",attrs={"class":"bloko-tag__section_text"})]
+    except:
+        tags = []
+    resume = {
+        "name":name,
+        "salary":salary,
+        "specialisation":spec,
+        "experience":exp,
+        "tags":tags,
+    }
+    return resume
+
+
+# In[189]:
+
+
+if __name__ == "__main__":
+    data1 = []
+    for a in get_links("журналист"):
+        data1.append(get_resume(a))
+        time.sleep(1)
+        with open("data.json","w",encoding="utf-8")as f:
+            json.dump(data1,f,indent = 4, ensure_ascii=False)
+
+
+# In[191]:
+
+
+df_resume1 = pd.json_normalize(data1)
+
+
+# In[539]:
+
+
+df_resume1.to_csv('resume_data_VKR.csv')
+
+
+# In[245]:
+
+
+res1 = df_resume1.copy()
+
+
+# In[475]:
+
+
+res1['tags'].dropna()
+
+
+# In[146]:
+
+
+matchPattern = r"([^A-Z]+)([A-Z]+)"
+replacePattern = r"\1 \2"
+res['specialisation'] = res['specialisation'].apply(lambda x: ''.join(x[j]if j not in[i for i in range(len(x)) if x[i]!=x.lower()[i]] else ' '+x[j]for j in range(len(x))).lstrip())
+res1['specialisation'] = res['specialisation'].apply(lambda x: x[14:])
+
+
+# In[246]:
+
+
+res1['specialisation'] = res1['specialisation'].apply(lambda x: x[14:])
+
+
+# In[247]:
+
+
+res1['spec_count'] = res1['specialisation'].apply(lambda x: sum(1 for c in x if c.isupper()))
+
+
+# In[248]:
+
+
+ss = ScaleStatistics(res1, ['spec_count'],
+                    normality_test=True)
+
+
+# In[200]:
+
+
+tt = res1.spec_count.value_counts()
+
+
+# In[201]:
+
+
+res1['spec_count'].value_counts()/res1['spec_count'].count()*100
+
+
+# In[538]:
+
+
+svar ='spec_count'
+
+plt.figure(figsize=(8,6))
+plt.hist(res1[svar].dropna(), color='pink')
+plt.xticks(rotation=90)
+plt.xlabel(f'Количество специализаций')
+plt.ylabel('Количество резюме')
+plt.savefig('spec_count.png')
+
+
+# In[249]:
+
+
+res1['spec1'] = res1['specialisation'].apply(lambda x: x.replace(" ", ""))
+
+
+# In[252]:
+
+
+res1['spec'] = res1['spec1'].apply(lambda x: re.sub(r'([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z])|[а-я](?=[А-Я]))|[a-z](?=[А-Я])', r'\1 ', x).split())
+
+
+# In[385]:
+
+
+listed11 = res11['tags'].tolist()
+
+
+# In[388]:
+
+
+flat_list1 = [item for sublist in listed11 for item in sublist]
+
+
+# In[384]:
+
+
+listed = res1['spec'].tolist()
+
+
+# In[266]:
+
+
+flat_list = [item for sublist in listed for item in sublist]
+
+
+# In[392]:
+
+
+counts = Counter(flat_list)
+print(counts)
+
+
+# In[403]:
+
+
+res_specs = pd.DataFrame.from_dict(counts, orient='index').reset_index()
+res_specs = res_specs.rename(columns={'index':'spec', 0:'count'})
+
+
+# In[412]:
+
+
+res_specs['%'] = res_specs['count'].apply(lambda x: x / 2117 * 100)
+res_specs = res_specs.sort_values('count', ascending=False)
+res_specs.index = range(1, len(res_specs)+1)
+
+
+# In[536]:
+
+
+res_specs.to_excel('res_specs.xlsx')
+
+
+# In[432]:
+
+
+counts1 = Counter(flat_list1)
+print(counts1)
+
+
+# In[434]:
+
+
+res_tags = pd.DataFrame.from_dict(counts1, orient='index').reset_index()
+res_tags = res_tags.rename(columns={'index':'tag', 0:'count'})
+
+
+# In[436]:
+
+
+res_tags['%'] = res_tags['count'].apply(lambda x: x / 2117 * 100)
+res_tags = res_tags.sort_values('count', ascending=False)
+res_tags.index = range(1, len(res_tags)+1)
+
+
+# In[476]:
+
+
+res_tags['% full'] = res_tags['count'].apply(lambda x: x / 1326 * 100)
+
+
+# In[486]:
+
+
+hist_res_tags = res_tags[res_tags['% full']>3.5]
+
+
+# In[488]:
+
+
+hist_res_tags['expertise'] = ""
+
+
+# In[491]:
+
+
+hist_res_tags
+
+
+# In[490]:
+
+
+hist_res_tags.loc[1,'expertise'] = 'TE'
+hist_res_tags.loc[2,'expertise'] = 'TE'
+hist_res_tags.loc[3,'expertise'] = 'TE'
+hist_res_tags.loc[4,'expertise'] = 'InnE'
+hist_res_tags.loc[5,'expertise'] = 'TE'
+hist_res_tags.loc[6,'expertise'] = 'IntE'
+hist_res_tags.loc[7,'expertise'] = 'TE'
+hist_res_tags.loc[8,'expertise'] = 'AE'
+hist_res_tags.loc[9,'expertise'] = 'InnE'
+hist_res_tags.loc[10,'expertise'] = 'TE'
+hist_res_tags.loc[11,'expertise'] = 'AE'
+hist_res_tags.loc[12,'expertise'] = 'IntE'
+hist_res_tags.loc[13,'expertise'] = 'AE'
+hist_res_tags.loc[14,'expertise'] = 'IntE'
+hist_res_tags.loc[15,'expertise'] = 'InnE'
+hist_res_tags.loc[16,'expertise'] = 'TE'
+hist_res_tags.loc[17,'expertise'] = 'TE'
+hist_res_tags.loc[18,'expertise'] = 'IntE'
+hist_res_tags.loc[19,'expertise'] = 'IntE'
+hist_res_tags.loc[20,'expertise'] = 'InnE'
+hist_res_tags.loc[21,'expertise'] = 'IntE'
+hist_res_tags.loc[22,'expertise'] = 'AE'
+hist_res_tags.loc[23,'expertise'] = 'TE'
+hist_res_tags.loc[24,'expertise'] = 'InnE'
+hist_res_tags.loc[25,'expertise'] = 'TE'
+hist_res_tags.loc[26,'expertise'] = 'TE'
+hist_res_tags.loc[27,'expertise'] = 'IntE'
+hist_res_tags.loc[28,'expertise'] = 'AE'
+hist_res_tags.loc[29,'expertise'] = 'IntE'
+hist_res_tags.loc[30,'expertise'] = 'IntE'
+hist_res_tags.loc[31,'expertise'] = 'IntE'
+hist_res_tags.loc[32,'expertise'] = 'TE'
+hist_res_tags.loc[33,'expertise'] = 'InnE'
+hist_res_tags.loc[34,'expertise'] = 'TE'
+hist_res_tags.loc[35,'expertise'] = 'TE'
+hist_res_tags.loc[36,'expertise'] = 'IntE'
+hist_res_tags.loc[37,'expertise'] = 'TE'
+hist_res_tags.loc[38,'expertise'] = 'TE'
+hist_res_tags.loc[39,'expertise'] = 'InnE'
+hist_res_tags.loc[40,'expertise'] = 'IntE'
+
+
+# In[497]:
+
+
+hist_res_tags.to_excel('hist_res_tags.xlsx')
+
+
+# In[ ]:
+
+
+exp_res_tags = hist_res_tags.copy()
+
+
+# In[531]:
+
+
+exp_res_tags = exp_res_tags.sort_values(['expertise', 'count'], ascending=[False, False])
+exp_res_tags.index = range(1, len(exp_res_tags)+1)
+
+
+# In[534]:
+
+
+exp_res_tags.to_excel('exp_res_tags.xlsx')
+
+
+# In[532]:
+
+
+vals = exp_res_tags['expertise'].tolist()
+colors = ["pink" if i == 'TE' else "lavenderblush" if i == 'InnE' else 'palevioletred' if i == 'IntE' else 'crimson' for i in vals]
+
+
+# In[535]:
+
+
+plt.figure(figsize=(15,15))
+plt.bar(exp_res_tags['tag'], exp_res_tags['% full'], color = colors)
+plt.ylabel(f'% в заполненных резюме')
+plt.xlabel('Навык')
+plt.xticks(rotation=90) 
+plt.savefig('exp_res_tags.png')
+
+
+# In[499]:
+
+
+exp_res_stat = exp_res_tags.groupby(['expertise']).sum()
+exp_res_stat
+
+
+# In[506]:
+
+
+exp_res_stat.sum()
+
+
+# In[500]:
+
+
+exp_res_stat.to_excel('exp_res_stat.xlsx')
+
+
+# In[502]:
+
+
+exp_res_stat['exp'] = exp_res_stat.index
+
+
+# In[503]:
+
+
+def make_autopct(values):
+    def my_autopct(pct):
+        total = sum(values)
+        val = int(round(pct*total/100.0))
+        return '{p:.2f}%  ({v:d})'.format(p=pct,v=val)
+    return my_autopct
+
+
+# In[505]:
+
+
+fig = plt.figure(figsize =(10, 7))
+plt.pie(exp_res_stat['count'], labels = exp_res_stat['exp'], autopct=make_autopct(exp_res_stat['count']), colors = ['olive', 'lightgoldenrodyellow', 'yellowgreen', 'darkolivegreen'])
+plt.savefig('exp_res_stat.png')
+
---
+++ VKR_Osipenko_Vacancy.py
@@ -0,0 +1,428 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+get_ipython().system('pip install fake_useragent')
+get_ipython().system('pip install requests')
+get_ipython().system('pip install bs4')
+get_ipython().system('pip install pyopenssl ndg-httpsclient pyasn1')
+
+
+# In[ ]:
+
+
+import requests
+import fake_useragent
+import time
+import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+from os import name
+from collections import Counter
+
+#Для обработки естественного языка
+import re
+import string
+import nltk
+from pymystem3 import Mystem
+from nltk.corpus import stopwords
+from string import punctuation
+from nltk import word_tokenize
+from nltk.probability import FreqDist
+
+
+# In[ ]:
+
+
+number_of_pages = 100
+
+job_title = ["Журналист"]
+for job in job_title:
+	data=[]
+	for i in range(number_of_pages):
+	    url = 'https://api.hh.ru/vacancies'
+	    par = {'text': job, 'area':'113','per_page':'10', 'page':i}
+	    r = requests.get(url, params=par)
+	    e=r.json()
+	    data.append(e)
+	    vacancy_details = data[0]['items'][0].keys()
+	    df = pd.DataFrame(columns= list(vacancy_details))
+	    ind = 0
+	    for i in range(len(data)):
+	    	for j in range(len(data[i]['items'])):
+	    		df.loc[ind] = data[i]['items'][j]
+	    		ind+=1
+	df.to_csv("Journalist.csv")
+
+
+# In[ ]:
+
+
+df['og_link'] = df['alternate_url'] + '?from=vacancy_search_list&hhtmFrom=vacancy_search_list'
+
+
+# In[ ]:
+
+
+vac_url = df['og_link'].tolist()
+
+
+# In[43]:
+
+
+def get_vacansy(link):
+    ua = fake_useragent.UserAgent()
+    data = requests.get(
+        url=link,
+        headers={"user-agent":ua.random}
+    )
+    if data.status_code != 200:
+        return
+    soup = BeautifulSoup(data.content, "lxml")
+    try:
+        name = soup.find(attrs={"class":"vacancy-title"}).find("h1").text
+    except:
+        name = ""
+    try:
+        tags = [tag.text for tag in soup.find(attrs={"class":"bloko-tag-list"}).find_all("span",attrs={"class":"bloko-tag__section_text"})]
+    except:
+        tags = []
+    resume = {
+        "name":name,
+        "tags":tags,
+    }
+    return resume
+
+
+# In[55]:
+
+
+if __name__ == "__main__":
+    dat1 = []
+    for a in vac_url:
+        dat1.append(get_vacansy(a))
+        time.sleep(1)
+        with open("data.json","w",encoding="utf-8")as f:
+            json.dump(dat1,f,indent = 4, ensure_ascii=False)
+
+
+# In[56]:
+
+
+df_vac = pd.json_normalize(dat1)
+
+
+# In[206]:
+
+
+df_vac.to_csv('vacancy_data_VKR.csv')
+
+
+# In[151]:
+
+
+vac_vac = df_vac.copy()
+
+
+# In[154]:
+
+
+vac_vac.tags = vac_vac.tags.apply(lambda x: np.nan if len(x)==0 else x)
+
+
+# In[155]:
+
+
+vac_vac.dropna()
+
+
+# In[80]:
+
+
+name_list = df_vac['name'].tolist()
+
+
+# In[89]:
+
+
+texx = ' '.join(name_list)
+texx = texx.replace('-', ' ')
+texx = texx.replace('/', ' ')
+texx
+
+
+# In[119]:
+
+
+mystem = Mystem() 
+russian_stopwords = stopwords.words("russian") + ['это', 'язык', 'умение', 'специалист', 'менеджер','отдел', 'навык', 'хотеть', 'делать', 'начинать', 'хотеться', 'мочь', 'который', 'весь', 'свой']
+def preprocess_text(text):
+    text = re.sub(r"\n", '', text)
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = text.translate(str.maketrans('', '', string.digits))
+    tokens = mystem.lemmatize(text.lower())
+    tokens = [token for token in tokens if token not in russian_stopwords              and token != " "               and token.strip() not in punctuation              and token not in russian_stopwords]
+    
+    text = " ".join(tokens)
+    
+    return text
+
+
+# In[120]:
+
+
+texx = preprocess_text(texx)
+
+
+# In[122]:
+
+
+texx_tokens = word_tokenize(texx)
+
+
+# In[123]:
+
+
+text1 = nltk.Text(texx_tokens)
+
+
+# In[124]:
+
+
+fdist = FreqDist(text1)
+
+
+# In[125]:
+
+
+fdist.most_common(20)
+
+
+# In[126]:
+
+
+vac_names = pd.DataFrame.from_dict(fdist, orient='index').reset_index()
+vac_names = vac_names.rename(columns={'index':'name', 0:'count'})
+
+
+# In[127]:
+
+
+vac_names['%'] = vac_names['count'].apply(lambda x: x / 577 * 100)
+vac_names = vac_names.sort_values('count', ascending=False)
+vac_names.index = range(1, len(vac_names)+1)
+
+
+# In[128]:
+
+
+vac_names[vac_names['%']>3]
+
+
+# In[204]:
+
+
+vac_names.to_excel('vac_names.xlsx')
+
+
+# In[129]:
+
+
+list_tags = df_vac['tags'].tolist()
+
+
+# In[134]:
+
+
+flat_list1 = [item for sublist in list_tags for item in sublist]
+
+
+# In[136]:
+
+
+counts2 = Counter(flat_list1)
+print(counts2)
+
+
+# In[137]:
+
+
+vac_tags = pd.DataFrame.from_dict(counts2, orient='index').reset_index()
+vac_tags = vac_tags.rename(columns={'index':'tag', 0:'count'})
+
+
+# In[138]:
+
+
+vac_tags['%'] = vac_tags['count'].apply(lambda x: x / 577 * 100)
+vac_tags = vac_tags.sort_values('count', ascending=False)
+vac_tags.index = range(1, len(vac_tags)+1)
+
+
+# In[161]:
+
+
+vac_tags['% full'] = vac_tags['count'].apply(lambda x: x / 498 * 100)
+
+
+# In[164]:
+
+
+new_vac = vac_tags[vac_tags['% full']>3]
+
+
+# In[166]:
+
+
+new_vac
+
+
+# In[165]:
+
+
+new_vac.to_excel('new_vac.xlsx')
+
+
+# In[205]:
+
+
+exp_vac_tags = vac_tags[vac_tags['% full']>3.5]
+
+
+# In[142]:
+
+
+exp_vac_tags['expertise'] = ""
+
+
+# In[144]:
+
+
+exp_vac_tags.loc[1,'expertise'] = 'TE'
+exp_vac_tags.loc[2,'expertise'] = 'TE'
+exp_vac_tags.loc[3,'expertise'] = 'TE'
+exp_vac_tags.loc[4,'expertise'] = 'IntE'
+exp_vac_tags.loc[5,'expertise'] = 'TE'
+exp_vac_tags.loc[6,'expertise'] = 'IntE'
+exp_vac_tags.loc[7,'expertise'] = 'AE'
+exp_vac_tags.loc[8,'expertise'] = 'IntE'
+exp_vac_tags.loc[9,'expertise'] = 'InnE'
+exp_vac_tags.loc[10,'expertise'] = 'TE'
+exp_vac_tags.loc[11,'expertise'] = 'IntE'
+exp_vac_tags.loc[12,'expertise'] = 'InnE'
+exp_vac_tags.loc[13,'expertise'] = 'AE'
+exp_vac_tags.loc[14,'expertise'] = 'TE'
+exp_vac_tags.loc[15,'expertise'] = 'TE'
+exp_vac_tags.loc[16,'expertise'] = 'IntE'
+exp_vac_tags.loc[17,'expertise'] = 'InnE'
+exp_vac_tags.loc[18,'expertise'] = 'AE'
+exp_vac_tags.loc[19,'expertise'] = 'AE'
+exp_vac_tags.loc[20,'expertise'] = 'AE'
+exp_vac_tags.loc[21,'expertise'] = 'AE'
+exp_vac_tags.loc[22,'expertise'] = 'TE'
+exp_vac_tags.loc[23,'expertise'] = 'TE'
+exp_vac_tags.loc[24,'expertise'] = 'AE'
+exp_vac_tags.loc[25,'expertise'] = 'AE'
+exp_vac_tags.loc[26,'expertise'] = 'IntE'
+exp_vac_tags.loc[27,'expertise'] = 'InnE'
+exp_vac_tags.loc[28,'expertise'] = 'InnE'
+exp_vac_tags.loc[29,'expertise'] = 'InnE'
+exp_vac_tags.loc[30,'expertise'] = 'InnE'
+exp_vac_tags.loc[31,'expertise'] = 'IntE'
+exp_vac_tags.loc[32,'expertise'] = 'TE'
+exp_vac_tags.loc[33,'expertise'] = 'TE'
+exp_vac_tags.loc[34,'expertise'] = 'IntE'
+exp_vac_tags.loc[35,'expertise'] = 'AE'
+exp_vac_tags.loc[36,'expertise'] = 'IntE'
+exp_vac_tags.loc[37,'expertise'] = 'InnE'
+exp_vac_tags.loc[38,'expertise'] = 'IntE'
+exp_vac_tags.loc[39,'expertise'] = 'InnE'
+exp_vac_tags.loc[40,'expertise'] = 'TE'
+exp_vac_tags.loc[41,'expertise'] = 'IntE'
+exp_vac_tags.loc[42,'expertise'] = 'IntE'
+
+
+# In[175]:
+
+
+exp_vac_stat = exp_vac_tags.groupby(['expertise']).sum()
+exp_vac_stat.to_excel('exp_vac_stat.xlsx')
+
+
+# In[183]:
+
+
+exp_vac_stat['exp'] = exp_vac_stat.index
+
+
+# In[186]:
+
+
+exp_vac_stat['count %'] = exp_vac_stat['count'].apply(lambda x: x / 2211 * 100)
+
+
+# In[190]:
+
+
+def make_autopct(values):
+    def my_autopct(pct):
+        total = sum(values)
+        val = int(round(pct*total/100.0))
+        return '{p:.2f}%  ({v:d})'.format(p=pct,v=val)
+    return my_autopct
+
+
+# In[197]:
+
+
+fig = plt.figure(figsize =(10, 7))
+plt.pie(exp_vac_stat['count'], labels = exp_vac_stat['exp'], autopct=make_autopct(exp_vac_stat['count']), colors = ['lightskyblue', 'steelblue', 'aliceblue', 'lightslategray'])
+plt.savefig('exp_vac_stat.png')
+
+
+# In[174]:
+
+
+exp_vac_stat['count'].sum()
+
+
+# In[172]:
+
+
+exp_vac_tags.groupby(['expertise']).count()
+
+
+# In[159]:
+
+
+exp_vac_tags = exp_vac_tags.sort_values(['expertise', 'count'], ascending=[False, False])
+exp_vac_tags.index = range(1, len(exp_vac_tags)+1)
+
+
+# In[160]:
+
+
+exp_vac_tags.to_excel('exp_vac_tags.xlsx')
+
+
+# In[147]:
+
+
+vals = exp_vac_tags['expertise'].tolist()
+colors = ["pink" if i == 'TE' else "lavenderblush" if i == 'InnE' else 'palevioletred' if i == 'IntE' else 'crimson' for i in vals]
+
+
+# In[203]:
+
+
+plt.figure(figsize=(15,15))
+plt.bar(exp_vac_tags['tag'], exp_vac_tags['% full'], color = colors)
+plt.ylabel(f'% в заполненных резюме')
+plt.xlabel('Навык')
+plt.xticks(rotation=90)
+plt.savefig('exp_vac_tags.png')
+
